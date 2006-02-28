cc1839eae4240faacc7aa7aa8b03482c8c532181
s_krustev <s_krustev@b78f87d0-8208-11de-b41d-d982dfe7be50>
2006-02-28 19:08:19 +0000

    branching to fix the logic operations

---
+++ branches/LOGICOP/src/tui.h
@@ -0,0 +1,158 @@
+//===========================================================================
+//                                                                          =
+//   This program is free software; you can redistribute it and/or modify   =
+//   it under the terms of the GNU General Public License as published by   =
+//   the Free Software Foundation; either version 2 of the License, or      =
+//   (at your option) any later version.                                    =
+// ------------------------------------------------------------------------ =
+//                  TTTTT    OOO    PPPP    EEEE    DDDD                    =
+//                  T T T   O   O   P   P   E       D   D                   =
+//                    T    O     O  PPPP    EEE     D    D                  =
+//                    T     O   O   P       E       D   D                   =
+//                    T      OOO    P       EEEEE   DDDD                    =
+// ------------------------------------------------------------------------ =
+//          $URL$
+//       Created: Thu Jun 17 2004
+//        Author: Svilen Krustev - s_krustev@yahoo.com
+//     Copyright: (C) 2004 by Svilen Krustev
+//   Description: Tell user interface classes
+//---------------------------------------------------------------------------
+//  Revision info
+//---------------------------------------------------------------------------                
+//      $Revision$
+//          $Date$
+//        $Author$
+//===========================================================================
+//      Comments :
+//===========================================================================
+
+#if !defined(TUI_H_INCLUDED)
+#define TUI_H_INCLUDED
+
+#include <wx/wx.h>
+#include <wx/spinbutt.h>
+#include "../tpd_common/ttt.h"
+#include "resourcecenter.h"
+
+namespace tui {
+   class getSize : public wxDialog {
+   public:
+      getSize(wxFrame *parent, wxWindowID id, const wxString &title, wxPoint pos );
+      wxString value() const {return _wxText->GetValue();};
+   private:   
+      wxTextCtrl* _wxText;
+   };
+
+   class getStep : public wxDialog {
+   public:
+      getStep(wxFrame *parent, wxWindowID id, const wxString &title, wxPoint pos, 
+                                                                     real init );
+      wxString value() const {return _wxText->GetValue();};
+   private:   
+      wxTextCtrl* _wxText;
+   };
+
+   class getGrid : public wxDialog {
+   public:
+      getGrid(wxFrame *parent, wxWindowID id, const wxString &title, wxPoint pos, 
+                                                   real ig0, real ig1, real ig2);
+      wxString grid0() const {return _wxGrid0->GetValue();};
+      wxString grid1() const {return _wxGrid1->GetValue();};
+      wxString grid2() const {return _wxGrid2->GetValue();};
+   private:   
+      wxTextCtrl* _wxGrid0;
+      wxTextCtrl* _wxGrid1;
+      wxTextCtrl* _wxGrid2;
+   };
+
+   class getCellOpen : public wxDialog {
+   public:
+      getCellOpen(wxFrame *parent, wxWindowID id, const wxString &title, wxPoint pos, wxString init);
+      wxString get_selectedcell() const {return _nameList->GetStringSelection();};
+      wxListBox*  _nameList;
+   };
+
+   class getCellRef : public wxDialog {
+   public:
+      getCellRef(wxFrame *parent, wxWindowID id, const wxString &title, wxPoint pos, wxString init);
+      wxString get_selectedcell() const {return _nameList->GetStringSelection();};
+      wxString get_angle() const {return _rotation->GetValue();};
+      bool  get_flip() const {return _flip->GetValue();};
+      wxTextCtrl* _rotation;
+      wxCheckBox* _flip;
+      wxListBox*  _nameList;
+   };
+
+   class getCellARef : public wxDialog {
+   public:
+      getCellARef(wxFrame *parent, wxWindowID id, const wxString &title, wxPoint pos, wxString init);
+      wxString get_selectedcell() const {return _nameList->GetStringSelection();};
+      wxString get_angle() const {return _rotation->GetValue();};
+      wxString get_col() const {return _numY->GetValue();};
+      wxString get_row() const {return _numX->GetValue();};
+      wxString get_stepX() const {return _stepX->GetValue();};
+      wxString get_stepY() const {return _stepY->GetValue();};
+
+      bool  get_flip() const {return _flip->GetValue();};
+      wxTextCtrl* _rotation;
+      wxTextCtrl* _numX;
+      wxTextCtrl* _numY;
+      wxTextCtrl* _stepX;
+      wxTextCtrl* _stepY;
+      wxCheckBox* _flip;
+      wxListBox*  _nameList;
+   };
+
+   class getTextdlg : public wxDialog {
+   public:
+      getTextdlg(wxFrame *parent, wxWindowID id, const wxString &title, wxPoint pos);
+      wxString    get_text()  const {return _text->GetValue();};
+      wxString    get_size()  const {return _size->GetValue();};
+      wxString    get_angle() const {return _rotation->GetValue();};
+      bool        get_flip() const  {return _flip->GetValue();};
+   protected:   
+      wxTextCtrl* _text;
+      wxTextCtrl* _size;
+      wxTextCtrl* _rotation;
+      wxCheckBox* _flip;
+   };
+
+   class sgSpinButton : public wxSpinButton {
+   public:
+      sgSpinButton(wxWindow *parent, wxTextCtrl* textW, const float step, 
+      const float min, const float max, const float value, const int prec) ;
+   private:
+      wxTextCtrl* _wxText;
+      float _step;
+      int   _prec;
+      void  OnSpin(wxSpinEvent&);
+      DECLARE_EVENT_TABLE();
+   };
+
+   class getGDSimport : public wxDialog {
+   public:
+                     getGDSimport(wxFrame *parent, wxWindowID id, const wxString &title,
+                                                                  wxPoint pos, wxString init);
+      wxString       get_selectedcell() const {return _nameList->GetStringSelection();};
+      bool           get_overwrite()    const {return _overwrite->GetValue();};
+      bool           get_recursive()    const {return _recursive->GetValue();};
+   private:
+      wxCheckBox*    _overwrite;
+      wxCheckBox*    _recursive;
+      wxListBox*     _nameList;
+   };
+
+   class getGDSexport : public wxDialog {
+   public:
+                     getGDSexport(wxFrame *parent, wxWindowID id, const wxString &title,
+                                                                  wxPoint pos, wxString init);
+      wxString       get_selectedcell() const {return _nameList->GetStringSelection();};
+      bool           get_recursive()    const {return _recursive->GetValue();};
+   private:
+      wxCheckBox*    _recursive;
+      wxListBox*     _nameList;
+   };
+
+}
+#endif
+
---
+++ branches/LOGICOP/tpd_DB/tedstd.h
@@ -0,0 +1,175 @@
+//===========================================================================
+//                                                                          =
+//   This program is free software; you can redistribute it and/or modify   =
+//   it under the terms of the GNU General Public License as published by   =
+//   the Free Software Foundation; either version 2 of the License, or      =
+//   (at your option) any later version.                                    =
+// ------------------------------------------------------------------------ =
+//                  TTTTT    OOO    PPPP    EEEE    DDDD                    =
+//                  T T T   O   O   P   P   E       D   D                   =
+//                    T    O     O  PPPP    EEE     D    D                  =
+//                    T     O   O   P       E       D   D                   =
+//                    T      OOO    P       EEEEE   DDDD                    =
+// ------------------------------------------------------------------------ =
+//           $URL$
+//  Creation date: Sun May 22 15:43:49 BST 2005
+//     Created by: Svilen Krustev - s_krustev@yahoo.com
+//      Copyright: (C) 2001-2005 by Svilen Krustev
+//    Description: wxWidget version
+//---------------------------------------------------------------------------
+//  Revision info
+//---------------------------------------------------------------------------                
+//      $Revision$
+//          $Date$
+//        $Author$
+//===========================================================================
+
+#ifndef TEDSTD_H_INCLUDED
+#define TEDSTD_H_INCLUDED
+
+#include <string>
+#include "../tpd_common/ttt.h"
+#ifndef WIN32
+#include "config.h"
+#endif
+#include "../src/gds_io.h"
+
+//==============================================================================
+// Toped DaTa (TDT) file markers
+//==============================================================================
+#define TED_LEADSTRING     "TED"
+#define tedf_REVISION      0x02
+#define tedf_TIMECREATED   0x03
+#define tedf_TIMEUPDATED   0x04
+#define tedf_DESIGN        0x80
+#define tedf_DESIGNEND     0x81
+#define tedf_CELL          0x82
+#define tedf_CELLEND       0x83
+#define tedf_LAYER         0x84
+#define tedf_CELLREF       0x85
+#define tedf_CELLAREF      0x86
+#define tedf_BOX           0x87
+#define tedf_POLY          0x88
+#define tedf_WIRE          0x89
+#define tedf_TEXT          0x8A
+#define tedf_LAYEREND      0x8B
+
+//==============================================================================   
+class PSegment {
+public:
+   PSegment() : _A(0), _B(0), _C(0) {};
+   PSegment(real A, real B, real C) : _A(A), _B(B), _C(C) {};
+   PSegment(TP,TP);
+   byte     crossP(PSegment, TP&);
+   bool     empty() {return ((0 == _A) && (0 == _B));};
+   PSegment& ortho(TP);
+   PSegment operator = (const PSegment s) {_A = s._A; _B = s._B; _C = s._C; return *this;};
+private:
+   real     _A, _B, _C;   
+};
+
+namespace layprop {
+   class DrawProperties;
+}
+
+namespace laydata {
+   typedef enum { sh_active, sh_deleted, sh_selected, sh_partsel, sh_merged } SH_STATUS;
+   typedef enum {
+      shp_OK         = 0x00,
+      shp_ident      = 0x01, // identical points removed
+      shp_line       = 0x02, // 3 points in line - middle removed
+      shp_clock      = 0x04, // poits reordered to get antoclockwise order
+      shp_box        = 0x08, // shape is a box
+      shp_null       = 0x10, // 0 area - points are not forming a polygon
+      shp_acute      = 0x20, // acute angle 
+      shp_cross      = 0x40, // self crossing sequence
+      shp_4pnts      = 0x80  // less than 4 points 
+   } shape_status;
+   
+   class TEDfile;
+   class tdtdata;
+   class editobject;
+   class tdtcell;
+   class tdtcellref;
+   class tdtdesign;
+   typedef  std::pair<tdtdata*, SGBitSet*>          selectDataPair;
+   typedef  std::list<selectDataPair>               dataList;
+   typedef  std::map<word, dataList*>               selectList;
+   typedef  std::list<tdtdata*>                     shapeList;
+   typedef  std::map<word,shapeList*>               atticList;
+   typedef  std::map<std::string, tdtcell*>         cellList;
+   typedef  cellList::const_iterator                refnamepair;
+   typedef  std::deque<const tdtcellref*>           cellrefstack;
+   typedef  std::list<word>                         usedlayList;
+   typedef  std::deque<editobject*>                 editcellstack;
+
+   //==============================================================================
+   class validator {
+   public:
+                           validator(const pointlist& plist) : _status(shp_OK), 
+                                                            _plist(plist) {};
+                           validator() : _status(shp_OK) {};
+      bool                 valid()           {return _status < shp_null;};
+      byte                 status()          {return _status;};
+      bool                 box()             {return _status & shp_box;};
+      pointlist&           get_validated()   {return _plist;};
+      virtual char*        failtype() = 0;
+      virtual tdtdata*     replacement() = 0;
+      virtual             ~validator() {};
+   protected:
+      byte                 _status;
+      pointlist            _plist;
+   };
+   
+//==============================================================================
+   class   TEDfile {
+   public:
+                           TEDfile(const char*); // for reading
+                           TEDfile(tdtdesign*, std::string&); // for writing
+      void                 closeF() {fclose(_file);};
+      void                 read();
+      void                 cleanup();
+      std::string          getString();
+      void                 putString(std::string str);
+      real                 getReal();
+      void                 putReal(const real);
+      byte                 getByte();
+      void                 putByte(const byte ch) {fputc(ch, _file);};
+      word                 getWord();
+      void                 putWord(const word);
+      int4b                get4b();
+      void                 put4b(const int4b);
+      TP                   getTP();
+      void                 putTP(const TP*);
+      CTM                  getCTM();
+      void                 putCTM(const CTM);
+      void                 registercellread(std::string, tdtcell*);
+      void                 registercellwritten(std::string);
+      bool                 checkcellwritten(std::string);
+      refnamepair          getcellinstance(std::string cellname);
+      void                 get_cellchildnames(nameList*);
+      bool                 status() const  {return _status;};
+      word                 numread() const {return _numread;};
+      tdtdesign*           design() const  {return _design;};
+      time_t               created() const {return _created;};
+      time_t               lastUpdated() const {return _lastUpdated;};
+      protected:
+      bool                 _status;
+      word                 _numread;
+   private:
+      void                 getTime();
+      void                 putTime();
+      void                 getRevision();
+      void                 putRevision();
+      long int             _position;
+      FILE*                _file;
+      word                 _revision;
+      word                 _subrevision;
+      time_t               _created;
+      time_t               _lastUpdated;
+      tdtdesign*           _design;
+      nameList             _childnames;
+   };
+}
+
+#endif
