4665399f28621bb3ed323783305762bc09b4b430
s_krustev <s_krustev@b78f87d0-8208-11de-b41d-d982dfe7be50>
2007-05-24 22:05:09 +0000

    Some cosmetics. No code altered

--- trunk/tpd_parser/ted_prompt.h
+++ trunk/tpd_parser/ted_prompt.h
@@ -121,23 +121,4 @@ namespace console {
       static wxMutex          _mutex; 
    };
 }
-# define YYLLOC_DEFAULT(Current, Rhs, N)				\
-    do									\
-      if (N)								\
-	{								\
-	  (Current).first_line   = YYRHSLOC (Rhs, 1).first_line;	\
-	  (Current).first_column = YYRHSLOC (Rhs, 1).first_column;	\
-	  (Current).last_line    = YYRHSLOC (Rhs, N).last_line;		\
-	  (Current).last_column  = YYRHSLOC (Rhs, N).last_column;	\
-	  (Current).filename  = YYRHSLOC (Rhs, N).filename;	\
-	}								\
-      else								\
-	{								\
-	  (Current).first_line   = (Current).last_line   =		\
-	    YYRHSLOC (Rhs, 0).last_line;				\
-	  (Current).first_column = (Current).last_column =		\
-	    YYRHSLOC (Rhs, 0).last_column;				\
-		(Current).filename = YYRHSLOC (Rhs, 0).filename; \
-	}								\
-    while (0)
 #endif
--- trunk/tpd_parser/tellyzer.h
+++ trunk/tpd_parser/tellyzer.h
@@ -42,6 +42,26 @@
 
 #define YYLTYPE parsercmd::yyltype
 
+#define YYLLOC_DEFAULT(Current, Rhs, N)                         \
+   do                                                           \
+      if (N)                                                    \
+      {                                                         \
+      (Current).first_line   = YYRHSLOC (Rhs, 1).first_line;    \
+      (Current).first_column = YYRHSLOC (Rhs, 1).first_column;  \
+      (Current).last_line    = YYRHSLOC (Rhs, N).last_line;     \
+      (Current).last_column  = YYRHSLOC (Rhs, N).last_column;   \
+      (Current).filename     = YYRHSLOC (Rhs, N).filename;      \
+      }                                                         \
+      else                                                      \
+      {                                                         \
+      (Current).first_line   = (Current).last_line   =          \
+                           YYRHSLOC (Rhs, 0).last_line;         \
+      (Current).first_column = (Current).last_column =          \
+                        YYRHSLOC (Rhs, 0).last_column;          \
+         (Current).filename = YYRHSLOC (Rhs, 0).filename;       \
+      }                                                         \
+   while (0)
+
 //-----------------------------------------------------------------------------
 // Some forward declarations
 //-----------------------------------------------------------------------------
