90ae2110401b919fd8d675e791d4290e7bf03d52
s_krustev <s_krustev@b78f87d0-8208-11de-b41d-d982dfe7be50>
2007-12-09 22:20:01 +0000

    First step to remove tedop.h & tedop.cpp. No longer required because BO-modified is taking over the self-crossing checks

--- trunk/tpd_common/tedop.h
+++ trunk/tpd_common/tedop.h
@@ -30,16 +30,10 @@
 #include <vector>
 #include <list>
 #include "ttt.h"
-#include "avl_def.h"
 
 namespace tedop {
-   //===========================================================================
-   // Bentley-Ottmann alghorithm related definitions      
-   int xyorder(const TP*, const TP*);
-   int yxorder(const TP*, const TP*);
-   float isLeft(const TP*, const TP*, const TP*);
-   //===========================================================================
-   class plysegment {
+
+/*   class plysegment {
    public:
                         plysegment(const TP*, const TP*, unsigned);
       unsigned          edge; // to keep track of adjacent edges
@@ -47,6 +41,8 @@ namespace tedop {
       const TP*         rP;
       plysegment*       above;
       plysegment*       below;
+   private:
+      int xyorder(const TP*, const TP*);
    };
    
    //===========================================================================
@@ -58,65 +54,6 @@ namespace tedop {
                        ~segmentlist();
    private:
       std::vector<plysegment*> segs;
-   };
-   
-   //===========================================================================
-   // Sweep line is using Adelson-Velskii & Landis balanced binary tree
-   //===========================================================================
-   class SweepLine {
-   public:
-                        SweepLine(const pointlist);
-                       ~SweepLine(); 
-     void               add(plysegment*);
-     void               remove(plysegment*);
-     int                intersect(plysegment*, plysegment*);
-   protected:
-      static int        compare_seg(const void*, const void*, void*);
-      bool              coincideOK(plysegment*, plysegment*, float, float);
-      float             getLambda( const TP*, const TP*, const TP*);
-      pointlist         _plist;
-      unsigned          _numv;          // number of vertices in the polygon
-      avl_table*        _tree;          //AVL tree
-   };     
-         
-   //===========================================================================
-   class Event {
-   public:
-                         Event(plysegment* seg) : _seg(seg) {};
-      virtual const TP* evertex() const = 0;
-      virtual bool      check_valid(SweepLine&) const = 0;
-      plysegment*        segment() const {return _seg;};
-      //! Return the edge number for this event
-      int                edgeNo() const {return _seg->edge;};
-      virtual            ~Event() {};
-   protected:   
-      plysegment*        _seg;
-   };
-   
-   class LEvent : public Event {
-   public:
-                        LEvent(plysegment* seg) : Event(seg) {};
-      const TP*         evertex() const {return _seg->lP;};
-      bool              check_valid(SweepLine&) const;
-   };
-   
-   class REvent : public Event {
-   public:
-                        REvent(plysegment* seg) : Event(seg) {};
-      const TP*         evertex() const {return _seg->rP;};
-      bool              check_valid(SweepLine&) const;
-   };
-   
-   typedef std::vector<Event*> eventlist;
-   //===========================================================================
-   class EventQueue {
-   public:
-                        EventQueue(const segmentlist& segments);
-      bool              check_valid(SweepLine&);
-                       ~EventQueue();
-   protected:   
-      static bool       E_compare( const Event* v1, const Event* v2 );
-      eventlist         equeue;
-   };
+   };*/
 }
 #endif
