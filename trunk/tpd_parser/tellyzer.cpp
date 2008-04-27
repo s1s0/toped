593871015ca11a792a99facee4e2c97851c47b1e
s_krustev <s_krustev@b78f87d0-8208-11de-b41d-d982dfe7be50>
2008-04-27 15:51:41 +0000

    Linux configuration and version changes

--- trunk/tpd_parser/tellyzer.cpp
+++ trunk/tpd_parser/tellyzer.cpp
@@ -2029,7 +2029,7 @@ void console::toped_logfile::init(const std::string logFileName, bool append)
       _file << LFH_SEPARATOR << std::endl;
       _file << LFH_HEADER    << std::endl;
       _file << LFH_SEPARATOR << std::endl;
-      _file << LFH_REVISION  << "0.9" << std::endl;
+      _file << LFH_REVISION  << "0.9.x" << std::endl;
       _file << LFH_ENVIRONM  << std::string(wxGetCwd().mb_str()) << std::endl;
       _file << LFH_TIMESTAMP << timec() << std::endl;
       _file << LFH_SEPARATOR << std::endl;
