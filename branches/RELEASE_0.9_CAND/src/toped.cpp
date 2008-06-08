8d1b6c6d6fc3f39387c09520c9ff95f1e9004b3c
s_krustev <s_krustev@b78f87d0-8208-11de-b41d-d982dfe7be50>
2008-06-08 00:22:37 +0000

    MBug 78 fix. Toped should not crash now if it can't obtain apropriate visual. It will issue an error message instead

--- branches/RELEASE_0.9_CAND/src/toped.cpp
+++ branches/RELEASE_0.9_CAND/src/toped.cpp
@@ -690,21 +690,20 @@ void tui::TopedFrame::initView()
    //---------------------------------------------------------------------------- 
    int gl_attrib[] = { 
                         WX_GL_RGBA             ,
-/*                        WX_GL_MIN_RED          , 2,
-                        WX_GL_MIN_GREEN        , 2,
-                        WX_GL_MIN_BLUE         , 2,
-                        WX_GL_MIN_ALPHA        , 2,
-                        WX_GL_MIN_ACCUM_RED    , 2,
-                        WX_GL_MIN_ACCUM_GREEN  , 2,
-                        WX_GL_MIN_ACCUM_BLUE   , 2,
-                        WX_GL_MIN_ACCUM_ALPHA  , 2,*/
+                        WX_GL_MIN_RED          , 8,
+                        WX_GL_MIN_GREEN        , 8,
+                        WX_GL_MIN_BLUE         , 8,
+/*                        WX_GL_MIN_ALPHA        , 8,
+                        WX_GL_MIN_ACCUM_RED    , 8,
+                        WX_GL_MIN_ACCUM_GREEN  , 8,
+                        WX_GL_MIN_ACCUM_BLUE   , 8,
+                        WX_GL_MIN_ACCUM_ALPHA  , 8,*/
                         WX_GL_DOUBLEBUFFER     ,
                         GL_NONE };
 //                         WX_GL_DEPTH_SIZE    , 1,
 //     int* gl_attrib = NULL; 
 
    _canvas = DEBUG_NEW LayoutCanvas(this, wxDefaultPosition, wxDefaultSize, gl_attrib);
-
    //----------------------------------------------------------------------------
    // The command line
    //----------------------------------------------------------------------------
@@ -727,12 +726,13 @@ void tui::TopedFrame::initView()
                                     LeftDockable(true).
                                     RightDockable(true)
                       );
-   _winManager.AddPane( _canvas,    wxAuiPaneInfo().
-                                    CentrePane().
-                                    MaximizeButton(false).
-                                    Floatable(true).
-                                    CloseButton(false)
-                      );
+   if (_canvas->initStatus())
+      _winManager.AddPane( _canvas,    wxAuiPaneInfo().
+                                       CentrePane().
+                                       MaximizeButton(false).
+                                       Floatable(true).
+                                       CloseButton(false)
+                        );
    //_winManager.AddPane(_GLstatus, wxAuiPaneInfo().Top().Floatable(false).//Fixed().
    //                               CloseButton(false).CaptionVisible(false).BestSize(wxSize(1000,30)));
    _winManager.AddPane(logpane,     wxAuiPaneInfo().
