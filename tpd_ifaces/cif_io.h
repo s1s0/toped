7f2f9c5130d37d8263ab5ffa1b8d5c704ec1222a
s_krustev <s_krustev@b78f87d0-8208-11de-b41d-d982dfe7be50>
2008-06-15 23:22:24 +0000

    Work on CIF parser

--- tpd_ifaces/cif_io.h
+++ tpd_ifaces/cif_io.h
@@ -27,6 +27,7 @@
 #if !defined(CIFIO_H_INCLUDED)
 #define CIFIO_H_INCLUDED
 
+#include "../tpd_common/ttt.h"
 #include <string>
 
 #define YYLTYPE CIFin::yyltype
@@ -70,7 +71,7 @@ primCommand          = polygonCommand       | boxCommand    | roundFlashCommand 
                        wireCommand          | layerCommand  | callCommand       | 
                        userExtensionCommand | commentCommand
 
-polygonCommand       = "P" path.
+polygonCommand       = "P" <*{blank}*> path.
 boxCommand           = "B" <*{blank}*> integer sep integer sep point [sep point]
 roundFlashCommand    = "R" integer sep point.
 wireCommand          = "W" integer sep path.
@@ -105,15 +106,95 @@ commentChar          = any ASCII character except "(" or ")".
 <* ... *> -> included in the parser (or the scanner) because existing CIF files are using this syntax, but
              it breaks the formal syntax definition of CIF
 
+===========================================================================================================
+The user extensions below - as described in http://www.rulabinsky.com/cavd/text/chapb.html
+(Steven M. Rubin Copyright © 1994)
+===========================================================================================================
+
+0 x y layer N name;           Set named node on specified layer and position
+0V x1 y1 x2 y2 ... xn yn;     Draw vectors
+2A "msg" T x y;               Place message above specified location
+2B "msg" T x y;               Place message below specified location
+2C "msg" T x y;               Place message centered at specified location
+2L "msg" T x y;               Place message left of specified location
+2R "msg" T x y;               Place message right of specified location
+4A lowx lowy highx highy;     Declare cell boundary
+4B instancename;              Attach instance name to cell
+4N signalname x y;            Labels a signal at a location
+9  cellname;                  Declare cell name
+91 instancename;              Attach instance name to cell
+94 label x y;                 Place label in specified location
+95 label length width x y;    Place label in specified area
+
 */
+   class CIFData {
+      public:
+                     CIFData(CIFData* last) : _last(last) {};
+      protected:
+         CIFData*    _last;
+   };
+
+   class CIFBox : public CIFData {
+      public:
+                     CIFBox(CIFData*, word, word, TP*, TP*);
+      protected:
+         word        _length;
+         word        _width;
+         TP*         _center;
+         TP*         _direction;
+   };
+
+   class CIFPoly : public CIFData {
+      public:
+                     CIFPoly(CIFData* last, pointlist*);
+      protected:
+         pointlist*  _poly;
+   };
+
+   class CIFLayer {
+   public:
+                     CIFLayer(std::string name, CIFLayer* last);
+      std::string    name()         {return _name;}
+      CIFLayer*      last()         {return _last;}
+      void           addBox(word, word, TP*, TP* direction = NULL);
+      void           addPoly(pointlist* poly);
+   private:
+      std::string    _name;
+      CIFLayer*      _last;
+      CIFData*       _first;
+   };
+
+   class CIFStructure  {
+   public:
+                     CIFStructure(word, CIFStructure*, word=1,word=1);
+      void           cellNameIs(std::string cellname) {_cellname = cellname;}
+      CIFLayer*      secureLayer(std::string);
+   private:
+      word           _ID;
+      CIFStructure*  _last;
+      word           _a;
+      word           _b;
+      std::string    _cellname;
+      CIFLayer*     _first;
+   };
 
    class   CIFFile {
-   public:
+      public:
                      CIFFile(std::string);
                     ~CIFFile();
       bool           status() {return _status;}
+      void           addStructure(word, word = 1, word = 1);
+      void           doneStructure();
+      void           addBox(word, word, TP*, TP* direction = NULL);
+      void           addPoly(pointlist*);
+      void           secureLayer(char*);
    protected:
       bool           _status;
+//      CIFStructure*  currentStructure() {return _first;}
+      CIFStructure*  _first;
+      CIFStructure*  _current;
+      CIFStructure*  _default;
+      CIFLayer*      _curlay;
    };
 
    struct yyltype {
