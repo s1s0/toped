ca2dc77826f48e2f72aa2b014ab6f261f670ebd0
s_krustev <s_krustev@b78f87d0-8208-11de-b41d-d982dfe7be50>
2008-10-19 22:39:46 +0000

    Updating the version records

--- branches/RELEASE_0.92_CAND/src/toped.cpp
+++ branches/RELEASE_0.92_CAND/src/toped.cpp
@@ -887,7 +887,7 @@ void tui::TopedFrame::OnQuit( wxCommandEvent& WXUNUSED( event ) ) {
 void tui::TopedFrame::OnAbout( wxCommandEvent& WXUNUSED( event ) ) {
     wxAboutDialogInfo info;
     info.SetName(wxT("Toped"));
-    info.SetVersion(wxT("0.9.x"));
+    info.SetVersion(wxT("0.9.2"));
     info.SetIcon(wxIcon( toped32x32_xpm ));
     info.SetWebSite(wxT("www.toped.org.uk"));
     info.SetDescription(wxT("Open source IC layout editor"));
