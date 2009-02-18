7345665b52e99ac4be128fbc5c39f082d5c73dc6
s_krustev <s_krustev@b78f87d0-8208-11de-b41d-d982dfe7be50>
2009-02-18 22:40:47 +0000

    Work on Bug 15241. An input parameter "techno" added to cifimport. Used only as a size of the text objects. 0 is allowed - then text objects are nor imported. UI dialogue and menu shortcuts updated accordingly.

--- trunk/src/toped.cpp
+++ trunk/src/toped.cpp
@@ -82,13 +82,13 @@ tui::CanvasStatus::CanvasStatus(wxWindow* parent, wxWindowID id ,
    SetBackgroundColour(wxColour(wxT("LIGHT_GRAY")));
    SetForegroundColour(wxColour(wxT("BLACK")));
    X_pos = DEBUG_NEW wxStaticText(this, wxID_ANY, wxT("0.00"), wxDefaultPosition,wxSize(120,-1),
-                                                   wxST_NO_AUTORESIZE | wxALIGN_RIGHT /*| wxST_ELLIPSIZE_END | */);
+                                                   wxST_NO_AUTORESIZE | wxALIGN_RIGHT );
    Y_pos = DEBUG_NEW wxStaticText(this, wxID_ANY, wxT("0.00"), wxDefaultPosition, wxSize(120,-1),
-                                                   wxST_NO_AUTORESIZE | wxALIGN_RIGHT /*| wxST_ELLIPSIZE_END | */);
+                                                   wxST_NO_AUTORESIZE | wxALIGN_RIGHT );
    _dX = DEBUG_NEW wxStaticText(this, wxID_ANY, wxT(""), wxDefaultPosition, wxSize(100,-1),
-                                                   wxST_NO_AUTORESIZE | wxALIGN_RIGHT /*| wxST_ELLIPSIZE_END | */);
+                                                   wxST_NO_AUTORESIZE | wxALIGN_RIGHT );
    _dY = DEBUG_NEW wxStaticText(this, wxID_ANY, wxT(""), wxDefaultPosition, wxSize(100,-1),
-                                                   wxST_NO_AUTORESIZE | wxALIGN_RIGHT /*| wxST_ELLIPSIZE_END | */);
+                                                   wxST_NO_AUTORESIZE | wxALIGN_RIGHT );
 
    _selected = DEBUG_NEW wxStaticText(this, wxID_ANY, wxT("0"), wxDefaultPosition, wxSize(40,-1),
                                                    wxST_NO_AUTORESIZE | wxALIGN_LEFT);
@@ -695,7 +695,7 @@ void tui::TopedFrame::initToolBars()
 
    //_resourceCenter->setToolBarSize(_tuihorizontal, ICON_SIZE_16x16);
   // _resourceCenter->setToolBarSize(_tuihorizontal, ICON_SIZE_32x32);
-   _status = DEBUG_NEW wxToolBar(this, wxID_ANY, wxDefaultPosition, wxSize(300, 30), wxTB_FLAT|wxTB_NODIVIDER|wxTB_HORIZONTAL);
+   _status = DEBUG_NEW wxToolBar(this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxTB_FLAT|wxTB_NODIVIDER|wxTB_HORIZONTAL);
 
    _GLstatus = DEBUG_NEW CanvasStatus(_status, ID_WIN_GLSTATUS ,
                                           wxDefaultPosition, wxDefaultSize,
@@ -1242,7 +1242,7 @@ void tui::TopedFrame::OnCIFimport(wxCommandEvent& WXUNUSED(event))
    wxString ost_int;
    ost_int << wxT("cifread(\"") << dlg2.GetDirectory() << wxT("/") <<dlg2.GetFilename() << wxT("\")");
    wxString ost;
-   ost << wxT("cifimport(") << ost_int << wxT(", getciflaymap(true), true, false );cifclose();");
+   ost << wxT("cifimport(") << ost_int << wxT(", getciflaymap(true), true, false, 0.0 );cifclose();");
    _cmdline->parseCommand(ost);
 //   SetStatusText(wxT("Stream ")+dlg2.GetFilename()+wxT(" imported"));
 }
@@ -1389,9 +1389,13 @@ void tui::TopedFrame::OnCIFtranslate(wxCommandEvent& WXUNUSED(event))
       if (NULL != laymap2save)
          USMap2wxString(laymap2save , wxlaymap2save);
       wxString ost;
+      double techno;
+      if (!dlg->getTechno().ToDouble(&techno))
+         techno = 0.0;
       ost << wxT("cifimport(\"") << dlg->getSelectedCell() << wxT("\" , ") << wxlaymap << wxT(",")
           << (dlg->getRecursive() ? wxT("true") : wxT("false")) << wxT(",")
-          << (dlg->getOverwrite() ? wxT("true") : wxT("false")) << wxT(");");
+          << (dlg->getOverwrite() ? wxT("true") : wxT("false")) << wxT(",")
+          << techno << wxT(");");
       if (NULL != laymap2save)
          ost << wxT("setciflaymap(")
              << wxlaymap2save << wxT(");");
