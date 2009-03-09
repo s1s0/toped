a1a10a8d931a7eef56b37cd5f08d783f865296cb
gaitukevich <gaitukevich@b78f87d0-8208-11de-b41d-d982dfe7be50>
2009-03-09 16:03:49 +0000

    Calibre parser - intermediate saving

--- tpd_ifaces/calbr_reader.cpp
+++ tpd_ifaces/calbr_reader.cpp
@@ -32,7 +32,33 @@
 #include "calbr_reader.h"
 #include "../tpd_common/outbox.h"
 
+Calbr::drcRuleCheck::drcRuleCheck(const std::string &name)
+		:_ruleCheckName(name)
+{
+}
 
+void	Calbr::drcRuleCheck::setTimeStamp(const std::string &timeStamp)
+{
+	_timeStamp = timeStamp;
+}
+
+void	Calbr::drcRuleCheck::setCurResCount(int curResCount)
+{
+	_curResCount = curResCount;
+}
+
+void	Calbr::drcRuleCheck::setOrigResCount(int origResCount)
+{
+	_origResCount = origResCount;
+}
+	
+void	Calbr::drcRuleCheck::addDescrString(const std::string & str)
+{
+	_descrStrings.push_back(str);
+}
+
+
+//-----------------------------------------------------------------------------
 Calbr::CalbrFile::CalbrFile(const std::string &fileName)
 {
 	_fileName = fileName;
@@ -63,7 +89,50 @@ Calbr::CalbrFile::~CalbrFile()
 
 bool Calbr::CalbrFile::parse()
 {
-	char str[512];
+	char ruleCheckName[512];
 
-	if (fgets(str, 512, _calbrFile)==NULL) return false;
+	// get drc Rule Check name
+	if (fgets(ruleCheckName, 512, _calbrFile)==NULL) return false;
+
+	Calbr::drcRuleCheck *ruleCheck = DEBUG_NEW Calbr::drcRuleCheck(ruleCheckName);
+	char tempStr[512];
+	char timeStamp[512];
+	long resCount, origResCount, descrStrCount;
+
+	//Get Current Results Count, Orginal Results Count and description strings count
+	if (fgets(tempStr, 512, _calbrFile)==NULL) return false;
+	sscanf(tempStr, "%ld %ld %ld %[^\n]\n",  &resCount, &origResCount, &descrStrCount, timeStamp);
+	ruleCheck->setCurResCount(resCount);
+	ruleCheck->setOrigResCount(origResCount);
+	ruleCheck->setTimeStamp(timeStamp);
+
+	//Get Description Strings
+	for(long i= 0; i < descrStrCount; i++)
+	{
+		if (fgets(tempStr, 512, _calbrFile)==NULL) return false;
+		ruleCheck->addDescrString(tempStr);
+	}
+	//Get Results
+
+	for(long i= 0; i < resCount; i++)
+	{
+		if (fgets(tempStr, 512, _calbrFile)==NULL) return false;
+		char type;
+		long ordinal;
+		short numberOfElem;
+		sscanf( tempStr, "%c %ld %hd", &type, &ordinal, &numberOfElem);
+		switch(type)
+		{
+			case 'p'	: 
+				for(short j =0; j< numberOfElem; j++)
+				{
+				}
+				break;
+						
+			case 'e'	: break;
+			default	: return false;
+		}
+	}
+	return true;
+
 }
