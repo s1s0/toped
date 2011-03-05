0c2af649414763964adf97f3aae29a970d228caf
krustev.svilen <krustev.svilen@b78f87d0-8208-11de-b41d-d982dfe7be50>
2011-03-05 21:10:19 +0000

    Revision numbers updated in the new branch

--- branches/RELEASE_0.9.71_CAND/tpd_ifaces/cif_io.cpp
+++ branches/RELEASE_0.9.71_CAND/tpd_ifaces/cif_io.cpp
@@ -614,7 +614,7 @@ CIFin::CifExportFile::CifExportFile(std::string fn, laydata::TdtCell* topcell,
    TpdTime timec(time(NULL));
 
    _file << "(              CIF   2.0       );"    << std::endl;
-   _file << "(        generator : Toped 0.9.x );"  << std::endl;
+   _file << "(        generator : Toped 0.9.7.1 );"  << std::endl;
    _file << "(             user : tbd );"          << std::endl;
    _file << "(          machine : tbd );"          << std::endl;
    _file << "(       time stamp : " << timec() << ");" << std::endl;
