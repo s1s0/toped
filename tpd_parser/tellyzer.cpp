2b542c1285f4346b44b2ddd5244b73cf91d50c46
krustev.svilen <krustev.svilen@b78f87d0-8208-11de-b41d-d982dfe7be50>
2011-03-05 21:10:19 +0000

    Revision numbers updated in the new branch

--- tpd_parser/tellyzer.cpp
+++ tpd_parser/tellyzer.cpp
@@ -2276,7 +2276,7 @@ void console::toped_logfile::init(const std::string logFileName, bool append)
       _file << LFH_SEPARATOR << std::endl;
       _file << LFH_HEADER    << std::endl;
       _file << LFH_SEPARATOR << std::endl;
-      _file << LFH_REVISION  << "0.9.x" << std::endl;
+      _file << LFH_REVISION  << "0.9.7.1" << std::endl;
       _file << LFH_ENVIRONM  << std::string(wxGetCwd().mb_str(wxConvFile )) << std::endl;
       _file << LFH_TIMESTAMP << timec() << std::endl;
       _file << LFH_SEPARATOR << std::endl;
