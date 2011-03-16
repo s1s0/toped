aa504d0c5362545b3534e98c7d19a0489f5eadad
krustev.svilen <krustev.svilen@b78f87d0-8208-11de-b41d-d982dfe7be50>
2011-03-16 02:07:57 +0000

    QuadTree template - initial version. Compiles and runs.

--- branches/DATA_TEMPLATE/tpd_DB/quadtree.h
+++ branches/DATA_TEMPLATE/tpd_DB/quadtree.h
@@ -16,7 +16,7 @@
 //           $URL$
 //        Created: Sun Mar 07 2004
 //     Originator: Svilen Krustev - skr@toped.org.uk
-//    Description: Database structure in the memory - clipping
+//    Description: QuadTree - supplementary classes
 //---------------------------------------------------------------------------
 //  Revision info
 //---------------------------------------------------------------------------
@@ -32,115 +32,31 @@
 
 namespace laydata {
 
-   class QTreeTmp;
-//==============================================================================
-   /*! QuadTree class implements the main clipping algorithm of toped. Its main
-      purpose is to speed-up the drawing of the database. All objects of type
-      TdtData or its derivatives - means all layout data - are stored into the
-      objects of QuadTree type. Each object of QuadTree class is responsible for a
-      rectangular area defined by the _overlap field. This area is dynamic and
-      updated with every added, moved or deleted layout object.\n Each QuadTree
-      object might be a parent of maximum four child objects of the same QuadTree
-      class, so that each of the children is responsible for one out of the
-      possible four sub-rectangles. Every layout object is fitted during the
-      construction into the smallest possible QuadTree object. The children are
-      created dynamically when a new object is about to fit into one of the four
-      possible sub-rectangles of the _overlap area. Child QuadTree objects are
-      stored into an quads[4] array and each of them is responsible by convention
-      for the NW(north-west), NE(north-east), SE(south-east) and SW(south-west)
-      sub-rectangles of the _overlap box.\n
-      The methods can be split on several groups:
-         - add a single layout object - add(), fitInTree()
-         - add a group of layout objects - put(), sort(), fitSubTree()
-         - object selection - selectInBox(), unselectInBox(), selectFromList(),
-           selectAll()
-         - design modification - deleteMarked()
-         - tree maintenance - validate(), fullValidate(), sort(), resort(),
-           tmpStore()
-            */
-   class QuadTree {
-   public:
-                           QuadTree();
-                           QuadTree(InputTdtFile* const, bool);
-                          ~QuadTree();
-      void                 openGlDraw(layprop::DrawProperties&, const DataList*, bool) const;
-      void                 openGlRender(tenderer::TopRend&, const DataList*) const;
-//      void                 visible_shapes(laydata::ShapeList*, const DBbox&, const CTM&, const CTM&, unsigned long&);
-      short                clipType(tenderer::TopRend&) const;
-      void                 motionDraw(const layprop::DrawProperties&, CtmQueue&) const;
-      void                 add(TdtData* shape);
-      TdtData*             addBox(const TP& p1, const TP& p2);
-      TdtData*             addPoly(PointVector& pl);
-      TdtData*             addPoly(int4b* pl, unsigned psize);
-      TdtData*             addWire(PointVector& pl, WireWidth w);
-      TdtData*             addText(std::string text, CTM trans);
-      void                 write(TEDfile* const) const;
-      void                 dbExport(DbExportFile&) const;
-      void                 psWrite(PSFile&, const layprop::DrawProperties&) const;
-      void                 selectInBox(DBbox&, DataList*, bool, word /*selmask = laydata::_lmall*/);
-      void                 selectFromList(DataList*, DataList*);
-      void                 selectAll(DataList*, word selmask = laydata::_lmall, bool mark = true);
-      void                 unselectInBox(DBbox&, DataList*, bool);
-      bool                 deleteMarked(SH_STATUS stat=sh_selected, bool partselect=false);
-      bool                 deleteThis(laydata::TdtData*);
-      void                 cutPolySelected(PointVector&, DBbox&, ShapeList**);
-      TdtData*             mergeSelected(TdtData*& shapeRef);
-/*      TdtData*             getfirstover(const TP);
-      TdtData*             getnextover(const TP, laydata::TdtData*, bool& check);*/
-      bool                 getObjectOver(const TP pnt, laydata::TdtData*& prev);
-      void                 validate();
-      bool                 fullValidate();
-      void                 resort(laydata::TdtData* newdata = NULL);
-      void                 resort(ShapeList&);
-      bool                 empty() const;
-      void                 freeMemory();
-      /*! Return the overlapping box*/
-      DBbox                overlap() const   {return _overlap;}
-      /*! Return the overlapping box*/
-      void                 vlOverlap(const layprop::DrawProperties&, DBbox&, bool) const;
-      /*! Mark the tree as invalid*/
-      void                 invalidate()      {_props._invalid = true;}
-      /*! Return the status of _invalid flag*/
-      bool                 invalid() const   {return _props._invalid;}
-   private:
-      friend class QTreeTmp;
-      typedef unsigned     ObjectIter;
+   typedef unsigned            QuadsIter;
+   template <typename DataT>   class QTreeTmpl;
+   typedef QTreeTmpl<TdtData>  QuadTree;
+
 #ifdef WIN32
-      #pragma pack() struct  QuadProps
+   #pragma pack() struct  QuadProps
 #else
-      struct __attribute__ ((__packed__)) QuadProps
+   struct __attribute__ ((__packed__)) QuadProps
 #endif
-      {
-                                QuadProps();
-         byte                   numSubQuads() const;
-         char                   getPosition(QuadIdentificators);
-         void                   addQuad(QuadIdentificators);
-         void                   removeQuad(QuadIdentificators);
-         void                   clearQuadMap() {_quadMap = 0;}
-         ObjectIter             _numObjects;
-         /*! Flag indicates that the container needs to be resorted*/
-         bool                   _invalid;
-      private:
-         char                   getNEQuad() const;
-         char                   getNWQuad() const;
-         char                   getSEQuad() const;
-         char                   getSWQuad() const;
-         byte                   _quadMap;
-      };
-      void                 sort(ShapeList&);
-      bool                 fitInTree(TdtData* shape);
-      char                 fitSubTree(const DBbox&, DBbox*);
-      void                 tmpStore(ShapeList& store);
-      byte                 biggest(int8b* array) const;
-      void                 updateOverlap(const DBbox& hovl);
-      byte                 sequreQuad(QuadIdentificators);
-      void                 removeQuad(QuadIdentificators);
-      DBbox                _overlap;//! The overlapping box of the quad
-      /*! A pointers to four child QuadTree structures*/
-      QuadTree**           _subQuads;
-      /*! Pointer to the first TdtData stored in this QuadTree*/
-      TdtData**            _data;
-      QuadProps            _props;
+   {
+                             QuadProps();
+      byte                   numSubQuads() const;
+      char                   getPosition(QuadIdentificators);
+      void                   addQuad(QuadIdentificators);
+      void                   removeQuad(QuadIdentificators);
+      void                   clearQuadMap() {_quadMap = 0;}
+      QuadsIter             _numObjects;
+      /*! Flag indicates that the container needs to be resorted*/
+      bool                   _invalid;
+   private:
+      char                   getNEQuad() const;
+      char                   getNWQuad() const;
+      char                   getSEQuad() const;
+      char                   getSWQuad() const;
+      byte                   _quadMap;
    };
 
    class QTreeTmp {
@@ -152,11 +68,12 @@ namespace laydata {
        void                putPoly(int4b* pl, unsigned psize);
        void                putWire(PointVector& pl,word w);
        void                putText(std::string text, CTM trans);
-       void                commit() {_trunk->resort(_data);}
+       void                commit();
    private:
       ShapeList            _data;
-      QuadTree*            _trunk;
+      QuadTree*           _trunk;
    };
+
 }
 
 #endif
