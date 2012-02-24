b910d274d11a09918efe44978d68bf2d56d7a691
yannick.heinrich <yannick.heinrich@gmail.com>
2012-02-24 16:37:46 +0000

    Change the CMakeLists files for Linux and Win32 (not tested) and add  preprocessing values __WXOSX_COCOA__ to compile on this specific platform for wx2.9 in resourcecenter.cpp and tui.cpp.

--- src/resourcecenter.cpp
+++ src/resourcecenter.cpp
@@ -83,7 +83,13 @@ wxMenu* tui::MenuItemHandler::buildPath(wxMenuBar *menuBar, const std::vector<st
       {
          // create at the left of Help menu
          menu = DEBUG_NEW wxMenu();
-         menuBar->Insert(menuBar->GetMenuCount()-1, menu, wxString(menuString.c_str(), wxConvUTF8));
+#ifdef __WXOSX_COCOA__
+	menuBar->Append(menu, wxString(menuString.c_str(), 
+	wxConvUTF8)); 
+#else
+	menuBar->Insert(menuBar->GetMenuCount()-1, menu, wxString(menuString.c_str(), wxConvUTF8));
+#endif
+
          menuID = menuBar->FindMenu(wxString(menuString.c_str(), wxConvUTF8));
       }
 
