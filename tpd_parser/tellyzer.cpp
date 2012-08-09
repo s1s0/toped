31d5527b4d5e4bc41fdd37917c6b2fe8e0728511
krustev.svilen <krustev.svilen@gmail.com>
2012-08-09 22:51:09 +0000

    Preventing an assert in some cases when DB is not created.

--- tpd_parser/tellyzer.cpp
+++ tpd_parser/tellyzer.cpp
@@ -2235,12 +2235,17 @@ int parsercmd::cmdMAIN::execute()
       if (EXEC_NEXT == retexec) retexec = a->execute();
       delete a;
    }
-   if (_dbUnsorted)
+
+   try
    {
-      cmdSTDFUNC* sortFunc = getIntFuncBody("$sort_db");
-      sortFunc->execute();
-      _dbUnsorted = false;
+      if (_dbUnsorted)
+      {
+         cmdSTDFUNC* sortFunc = getIntFuncBody("$sort_db");
+         sortFunc->execute();
+         _dbUnsorted = false;
+      }
    }
+   catch (EXPTN&) {return EXEC_ABORT;}
    return retexec;
 }
 
