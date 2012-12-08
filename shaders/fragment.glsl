eb679d5afac5a2a815d1eaed38c8549dc3bc2a04
krustev.svilen <krustev.svilen@gmail.com>
2012-12-08 17:47:59 +0000

    Two shader files added to the project "officially". Also a tiny correction in the vertex shader to fix a compile error (not flagged by AMD).

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -1,4 +1,29 @@
-// Fragment Shader � file "minimal.frag"
+//===========================================================================
+//                                                                          =
+//   This program is free software; you can redistribute it and/or modify   =
+//   it under the terms of the GNU General Public License as published by   =
+//   the Free Software Foundation; either version 2 of the License, or      =
+//   (at your option) any later version.                                    =
+// ------------------------------------------------------------------------ =
+//                  TTTTT    OOO    PPPP    EEEE    DDDD                    =
+//                  T T T   O   O   P   P   E       D   D                   =
+//                    T    O     O  PPPP    EEE     D    D                  =
+//                    T     O   O   P       E       D   D                   =
+//                    T      OOO    P       EEEEE   DDDD                    =
+//                                                                          =
+//   This file is a part of Toped project (C) 2001-2009 Toped developers    =
+// ------------------------------------------------------------------------ =
+//           $URL$
+//        Created: Sat Dec  8 2012
+//     Originator: Svilen Krustev - skr@toped.org.uk
+//    Description: GLSL Fragment shader
+//---------------------------------------------------------------------------
+//  Revision info
+//---------------------------------------------------------------------------
+//      $Revision$
+//          $Date$
+//        $Author$
+//===========================================================================
 
 #version 330
 
@@ -18,7 +43,7 @@ void main(void)
       uvec2 ufCoord = uvec2(gl_FragCoord.x, gl_FragCoord.y);
       uint index = 31u - (ufCoord.y % 32u);
       uint mask  = uint(0x80000000) >> (ufCoord.x % 32u);
-      dropThePixel = !bool(in_Stipple[index+1] & mask);
+      dropThePixel = !bool(in_Stipple[index+uint(1)] & mask);
    }
    if (dropThePixel)
       discard;
