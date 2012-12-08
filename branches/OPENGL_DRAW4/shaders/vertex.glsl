3b563c0a08fba6480fb6fb062a14bd1a363da2a1
krustev.svilen@gmail.com <krustev.svilen@gmail.com@b78f87d0-8208-11de-b41d-d982dfe7be50>
2012-12-08 17:47:59 +0000

    Two shader files added to the project "officially". Also a tiny correction in the vertex shader to fix a compile error (not flagged by AMD).

--- branches/OPENGL_DRAW4/shaders/vertex.glsl
+++ branches/OPENGL_DRAW4/shaders/vertex.glsl
@@ -1,4 +1,29 @@
-// Vertex Shader � file "minimal.vert"
+//===========================================================================
+//                                                                          =
+//   This program is free software; you can redistribute it and/or modify   =
+//   it under the terms of the GNU General Public License as published by   =
+//   the Free Software Foundation; either version 2 of the License, or      =
+//   (at your option) any later version.                                    =
+// ------------------------------------------------------------------------ =
+//                  TTTTT    OOO    PPPP    EEEE    DDDD                    =
+//                  T T T   O   O   P   P   E       D   D                   =
+//                    T    O     O  PPPP    EEE     D    D                  =
+//                    T     O   O   P       E       D   D                   =
+//                    T      OOO    P       EEEEE   DDDD                    =
+//                                                                          =
+//   This file is a part of Toped project (C) 2001-2009 Toped developers    =
+// ------------------------------------------------------------------------ =
+//           $URL$
+//        Created: Sat Dec  8 2012
+//     Originator: Svilen Krustev - skr@toped.org.uk
+//    Description: GLSL Vertex shader
+//---------------------------------------------------------------------------
+//  Revision info
+//---------------------------------------------------------------------------
+//      $Revision$
+//          $Date$
+//        $Author$
+//===========================================================================
 
 #version 330
 
