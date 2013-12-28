a521fa933e5aa7853fadc23ff3b0094d0fe6cae2
krustev.svilen <krustev.svilen@gmail.com>
2013-12-28 02:27:24 +0000

    Yet another icon change. Cleaning unused and renaming.

---
+++ icons/48x48/boza.cmd
@@ -0,0 +1,13 @@
+svn mv box.png  new_box.png 
+svn mv poly.png  new_poly.png 
+svn mv wire.png  new_wire.png 
+svn mv text.png  new_text.png 
+svn mv rotate_left.png rotate_ccw.png 
+svn mv rotate_right.png rotate_cw.png 
+svn mv flipx.png flip_hory.png 
+svn mv flipy.png flip_vert.png 
+svn rm new.png 
+svn rm open.png 
+svn rm save.png 
+svn rm redo.png
+ 
